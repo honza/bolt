0b99797a1190d3acbcf65cc409c2eee0c75c5b59
Honza Pokorny <me@honza.ca>
2011-05-05 11:42:39 -0300

    Add install script.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Dependencies:
+# Bolt
+
+### Installation
+
+To install all the dependencies on an Ubuntu server, you can use the
+`install.sh` script. Run it as `root`.
+
+### Dependencies:
 
 * Node.js
 * npm
---
+++ install.sh
@@ -0,0 +1,28 @@
+sudo apt-get install git-core g++ curl libssl-dev apache2-utils -y
+cd ~
+
+git clone git://github.com/joyent/node.git
+git clone git://github.com/antirez/redis.git
+git clone git://github.com/isaacs/npm.git
+
+cd ~/node
+git checkout v0.4.7
+./configure
+make
+sudo make install
+
+cd ~/redis/src
+
+git checkout 2.2.5
+./configure
+make
+sudo make install
+
+cd ~/npm
+sudo make install
+
+npm install coffee-script
+npm install express
+npm install jade
+npm install hiredis redis
+npm install socket.io
